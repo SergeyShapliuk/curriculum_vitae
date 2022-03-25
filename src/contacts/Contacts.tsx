8c0e37762c21be3b5ef7aa0b4641a0fe17b77928
SergeyShapliuk <sergeshapluk@gmail.com>
2022-03-25 14:57:36 +0300

    connect contacts form

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -4,15 +4,13 @@ import styleContainer from "../common/styles/Container.module.scss";
 import Title from "../common/Title/Title";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope} from "@fortawesome/free-solid-svg-icons/faEnvelope";
-import {faLocationDot} from "@fortawesome/free-solid-svg-icons/faLocationDot";
 import {faPhone} from "@fortawesome/free-solid-svg-icons/faPhone";
-import {faMapLocation} from "@fortawesome/free-solid-svg-icons/faMapLocation";
 import {faGlobe} from "@fortawesome/free-solid-svg-icons/faGlobe";
+import ContactsForm from "./ContactsForm";
 
 
 
 function Contacts() {
-
 
     return (
         <div id={"contacts"} className={s.contactsBlock}>
@@ -27,10 +25,11 @@ function Contacts() {
                     </ul>
                 </div>
                 <div className={s.contactForm}>
-                    <input type={"text"} name={"name"} placeholder={' Your name'}/>
-                    <input type={"text"} name={"email"} placeholder={' Your email'}/><br/>
-                    <textarea name={"message"} placeholder={' Your message'}/><br/>
-                    <button>Send</button>
+                    {/*<input type={"text"} id={"name"} name={"name"} placeholder={' Your name'}/>*/}
+                    {/*<input type={"text"} name={"email"} placeholder={' Your email'}/><br/>*/}
+                    {/*<textarea name={"message"} placeholder={' Your message'}/><br/>*/}
+                    {/*<button>Send</button>*/}
+                    <ContactsForm/>
                 </div>
             </div>
         </div>
---
+++ src/contacts/ContactsForm.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {useFormik} from "formik";
+import axios from "axios";
+
+type FormValuesType = {
+    email?: string
+    name?: string
+    message?: string
+}
+const ContactsForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            name: '',
+            message: '',
+        },
+        validate: (values: FormValuesType) => {
+            const errors: FormValuesType = {}
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+            if (!values.name) {
+                errors.name = 'Name required';
+            } else if (values.name.length < 2) {
+                errors.name = 'Must be name contain more characters';
+            }
+            if (!values.message) {
+                errors.message = 'Message required';
+            } else if (values.message.length < 7) {
+                errors.message = 'The message must contain more characters';
+            }
+            return errors;
+        },
+        onSubmit: async (values: FormValuesType) => {
+            formik.resetForm()
+debugger
+           await axios.post("https://server-smtp-node-js.herokuapp.com/sendMessage", values
+            ).then(res=>alert("Your message has been send"))
+               .catch(err=>alert("Error"))
+        }
+    })
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <input type="email"
+                       placeholder={'Your email'}
+                       {...formik.getFieldProps("email")}/>
+                {formik.touched.email && formik.errors.email ?
+                    <div style={{color: "orange"}}>{formik.errors.email}</div> : null}
+                <input type="text"
+                       placeholder={' Your name'}
+                       {...formik.getFieldProps("name")}/>
+                {formik.touched.name && formik.errors.name ?
+                    <div style={{color: "orange"}}>{formik.errors.name}</div> : null}
+                <input type="textarea"
+                       placeholder={"Your message"}
+                       {...formik.getFieldProps("message")}/>
+                {formik.touched.message && formik.errors.message ?
+                    <div style={{color: "orange"}}>{formik.errors.message}</div> : null}
+                <button type="submit">
+                    Send
+                </button>
+            </form>
+        </div>
+    )
+}
+export default ContactsForm;
