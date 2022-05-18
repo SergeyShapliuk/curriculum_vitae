a384471484bed8cd898d504b5e3b97c23f351848
SergeyShapliuk <sergeshapluk@gmail.com>
2022-05-18 12:05:27 +0300

    add new project

--- src/myWorks/MyWorks.tsx
+++ src/myWorks/MyWorks.tsx
@@ -6,6 +6,7 @@ import todolistImage from '../assets/image/todolistImage.jpg'
 import socialNetImage from '../assets/image/socialNetworkImage.jpg'
 import counterImage from '../assets/image/image-for-min.png'
 import cardsImage from '../assets/image/cardsImage.jpg'
+import shopImage from '../assets/image/imageScreen.png'
 import Title from "../common/Title/Title";
 import {Zoom} from "react-awesome-reveal";
 
@@ -27,6 +28,10 @@ function MyWorks() {
         color: 'blue',
         backgroundImage: `url(${cardsImage})`,
     };
+    const shop = {
+        color: 'blue',
+        backgroundImage: `url(${shopImage})`,
+    };
 
     return (
         <div id={"my_works"} className={s.myWorksBlock}>
@@ -35,17 +40,25 @@ function MyWorks() {
                 <div className={s.myWorks}>
                     <Zoom cascade={true} damping={.01} className={s.zoomMyWorks}>
                         <MyWork style={socialNetwork} title={'Social Network'}
-                                description={'Used technologies:  JavaScript, TypeScript, React, Redux, Redux-thunk, Redux-form, Axios, Reselect. '}
-                                hrefDemo={' https://sergeyshapliuk.github.io/social_network/'} hrefCode={'https://github.com/SergeyShapliuk/social_network'}/>
+                                description={'Used technologies:  JavaScript, TypeScript, React, Redux, Redux-thunk, Redux-form, Axios, Reselect.'}
+                                hrefDemo={' https://sergeyshapliuk.github.io/social_network/'}
+                                hrefCode={'https://github.com/SergeyShapliuk/social_network'}/>
                         <MyWork style={todolist} title={'Todo-List'}
                                 description={'Used technologies:  JavaScript, TypeScript, React, Redux, Material UI, SCSS. '}
-                                hrefDemo={'https://sergeyshapliuk.github.io/it-incubator-todolist-ts-start/'} hrefCode={'https://github.com/SergeyShapliuk/it-incubator-todolist-ts'}/>
+                                hrefDemo={'https://sergeyshapliuk.github.io/it-incubator-todolist-ts-start/'}
+                                hrefCode={'https://github.com/SergeyShapliuk/it-incubator-todolist-ts'}/>
                         <MyWork style={counter} title={'Counter'}
                                 description={'Used technologies: JavaScript, TypeScript, React, Redux. '}
-                                hrefDemo={' https://counter-pet-project.herokuapp.com'} hrefCode={'https://github.com/SergeyShapliuk/counter'}/>
+                                hrefDemo={' https://counter-pet-project.herokuapp.com'}
+                                hrefCode={'https://github.com/SergeyShapliuk/counter'}/>
                         <MyWork style={cards} title={'Cards app'}
-                                description={'Technologies: React, Redux, React-router domV6, Redux-thunk, Axios, Formik, Material UI. Creating learning app in team with using git, work with axios(CRUD). '}
-                                hrefDemo={' https://sergeyshapliuk.github.io/team-project/'} hrefCode={'https://github.com/SergeyShapliuk/team-project'}/>
+                                description={'Technologies: React, Redux, React-router domV6, Redux-thunk, Axios, Formik, Material UI. Creating learning app in team with using git, work with axios(CRUD).'}
+                                hrefDemo={' https://sergeyshapliuk.github.io/team-project/'}
+                                hrefCode={'https://github.com/SergeyShapliuk/team-project'}/>
+                        <MyWork style={shop} title={'Shops'}
+                                description={'Technologies: React, Redux, React-router domV6, Redux-thunk, GraphQL,SCSS. The task was performed as a test.Using GraphQL(apollo client.'}
+                                hrefDemo={' https://github.com/SergeyShapliuk/shop-test'}
+                                hrefCode={'https://github.com/SergeyShapliuk/shop-test'}/>
                     </Zoom>
                 </div>
             </div>
